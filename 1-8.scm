0dd114caaaac0c158e8a290d7295b562947ccc90
brickgao <brickgao@gmail.com>
2014-03-15 21:01:02 +0800

    add 1-8

---
+++ 1-8.scm
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (curt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (curt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (/ 
+   (+ 
+    (/ x 
+       (* guess guess)) 
+    (* 2 guess)) 
+   3))
+
+(define (good-enough? guess x)
+  (< (abs (- (/ (improve guess x) guess) 1)) 0.001))
