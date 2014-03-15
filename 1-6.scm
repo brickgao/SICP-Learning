74f4f818155b5b52e808a981e189bba0b9a529e5
brickgao <brickgao@gmail.com>
2014-03-15 20:46:49 +0800

    add 1-6

---
+++ 1-6.scm
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (/ (improve guess x) guess) - 1)) 0.001))
