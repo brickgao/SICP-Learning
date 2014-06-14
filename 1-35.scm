d2dc1d9bdb9895ffeef50cf1446ecc5d3a74cb28
brickgao <brickgao@gmail.com>
2014-06-14 16:33:07 +0800

    add 1-35

---
+++ 1-35.scm
@@ -0,0 +1,20 @@
+#lang racket
+
+(define tolerance  0.000001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (f x)
+  (+ 1
+     (/ 1 x)))
+
+(fixed-point f (exact->inexact 1))
+
