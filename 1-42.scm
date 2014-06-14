7a0feb2905d565a1b97ba948378ca246516df5d4
brickgao <brickgao@gmail.com>
2014-06-14 22:25:39 +0800

    add 1-42

---
+++ 1-42.scm
@@ -0,0 +1,15 @@
+#lang racket
+
+(provide compose)
+
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (square x)
+  (* x x))
+
+(define (inc x)
+  (+ x 1))
+
+((compose square inc) 6)
