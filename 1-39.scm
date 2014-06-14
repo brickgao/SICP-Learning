d47438fbc6f07c4853b06f65aee8a8df4da3f252
brickgao <brickgao@gmail.com>
2014-06-14 21:01:19 +0800

    add 1-39

--- 1-39.scm
+++ 1-39.scm
@@ -1,1 +1,22 @@
 #lang racket
+
+(define (cont-frac n d k)
+  (define (cont-frac-step i)
+    (if (= i k)
+        (/ (n i)
+           (d i))
+        (/ (n i)
+           (- (d i)
+              (cont-frac-step (+ i 1))))))
+  (cont-frac-step 1))
+
+(define (pow x y)
+  (if (= y 0)
+      1
+      (* x (pow x (- y 1))))) 
+
+(define (tan-cf x k)
+  (cont-frac (lambda (i) (pow x i))
+             (lambda (i) (- (* i 2)
+                            1))
+             k))
