bca30ad91e556bacb567f9ed295588a197b10e36
brickgao <brickgao@gmail.com>
2014-06-14 23:00:24 +0800

    add 1-43

---
+++ 1-43.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+(require "1-42.scm")
+
+(define (repeated f n)
+  (if (<= n 1)
+      (lambda (x)
+        (f x))
+      (compose f (repeated f (- n 1)))))
+
+(define (square x)
+  (* x x))
+
+((repeated square 2) 5)
