2bae20154713f5ad73bc2eada4ef9a883df20643
brickgao <brickgao@gmail.com>
2014-06-18 17:38:12 +0800

    add 2-5

---
+++ 2-5.scm
@@ -0,0 +1,29 @@
+#lang racket
+
+(define (pow x y)
+  (if (= y 0)
+      1
+      (* x
+         (pow x (- y 1)))))
+
+(define (cons x y)
+  (* (pow 2 x)
+     (pow 3 y)))
+
+(define (car z)
+  (define (divide? x)
+    (= 0
+       (remainder x 2)))
+  (if (divide? z)
+      (+ 1
+         (car (/ z 2)))
+      0))
+
+(define (cdr z)
+  (define (divide? x)
+    (= 0
+       (remainder x 3)))
+  (if (divide? z)
+      (+ 1
+         (cdr (/ z 3)))
+      0))
